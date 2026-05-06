2e1d147ecb1af88cd3c558b0f49ca1d9748ad85f
progamadorvinicius <vinicius.brow244@gmail.com>
2026-05-06 02:15:34 +0100

    terminando

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const cards = document.querySelectorAll(".card")
-cards.forEach(card =>)
+const cards = document.querySelectorAll(".card") cards.forEach(card => {
+    card.addEventListener("mousemove" ,(e) => {
+        const rect = card.()
+        const x = e.clientx - rect.left;
+        const y = e.clienty - rect.top;
+
+        const rotatex = ((y/rect.height) -0.5) *15;
+        const rotatey = ((x/rect.height) -0.5) *-15;
+
+        card.style.transform = 'rotatex($'{rotatex}de
+    });
+})
